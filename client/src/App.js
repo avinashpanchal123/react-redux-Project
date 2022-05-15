74bc230851c095043a42e00cebd78de8afec5ebe
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-05-15 13:32:50 +0530

    created Home page banner

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
 import Header from './components/layout/Header';
+import Home from './components/home/Home';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
   <>
   <Header/>
+  <Routes>
+  <Route exact path='/' element={<Home />}/>
+  </Routes>
+  
   </>
   );
 }
---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+  return (
+    <>
+    <div className="banner">
+        <p>Welcome to Ecommerce</p>
+        <h1>FIND AMAZING RPODUCTS BELOW</h1>
+
+        <a href="#container">
+            <button> 
+                Scroll 
+            </button>
+        </a>
+    </div>
+    
+    </>
+  )
+}
+
+export default Home
